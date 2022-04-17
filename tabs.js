3202532ff3cdc46bba28a96aa7361b1b11b68e86
Shane Quick <shanequickdev@gmail.com>
2022-04-16 22:54:56 -0500

    Add event listeners for all destination tabs.

--- tabs.js
+++ tabs.js
@@ -1,19 +1,90 @@
-const fetchJson = async () => {
+ async function fetchJson() {
     try {
         const data = await fetch("./data.json");
-        const response = await data.json();
-
-        return response;
+        return await data.json();
     } catch (error) {
         console.log(error);
     }
  };
 
-const data = fetchJson();
+ const fillPlanetData = async (planet) => {
+    const article = document.getElementsByClassName("destination-info")[0];
+    const meta = document.getElementsByClassName("destination-meta")[0];
+    const title = article.getElementsByTagName("h2")[0];
+    const desscription = article.getElementsByTagName("p")[0];
+    const distance = meta.getElementsByTagName("p")[0];
+    const travel = meta.getElementsByTagName("p")[1];
+    const picture = document.getElementsByClassName("grid-container--destination")[0].getElementsByTagName("picture")[0];
+    const pictureSource = picture.getElementsByTagName("source")[0];
+    const pictureImage = picture.getElementsByTagName("img")[0];
+
+    title.innerText = planet.name;
+    desscription.innerText = planet.description;
+    distance.innerText = planet.distance;
+    travel.innerText = planet.travel;
+    pictureSource.srcset = planet.images.webp;
+    pictureImage.src = planet.images.png;
+ }
+
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = document.querySelectorAll('[role="tab"]');
 
 tabList.addEventListener('keydown', (e) => {
-    console.log(tabList);
-    console.log(tabs);
+    const keyLeft = 37;
+    const keyRight = 39;
+    const currentTab = tabList.getElementsByClassName("active")[0];
+
+    if (e.keyCode === keyLeft) {
+        if (currentTab.previousElementSibling) {
+            currentTab.classList.remove("active");
+            currentTab.previousElementSibling.classList.add("active");
+        } else {
+            currentTab.classList.remove("active");
+            tabList.children[tabList.children.length - 1].classList.add("active");
+        }
+    } else if (e.keyCode === keyRight) {
+        if (currentTab.nextElementSibling) {
+            currentTab.classList.remove("active");
+            currentTab.nextElementSibling.classList.add("active");
+        } else {
+            currentTab.classList.remove("active");
+            tabList.children[0].classList.add("active");
+        }
+    }
+});
+
+tabs.forEach(tab => {
+    switch (tab.innerText)
+    {
+        case "MOON":
+            tab.addEventListener("focus", async () => {
+                const data = await fetchJson();
+                const moon = data.destinations[0];
+                fillPlanetData(moon);
+            });
+            break;
+        case "MARS":
+            tab.addEventListener("focus", async () => {
+                const data = await fetchJson();
+                const mars = data.destinations[1];
+                fillPlanetData(mars);
+            });
+            break;
+        case "EUROPA":
+            tab.addEventListener("focus", async () => {
+                const data = await fetchJson();
+                const europa = data.destinations[2];
+                fillPlanetData(europa);
+            });
+            break;
+        case "TITAN":
+            tab.addEventListener("focus", async () => {
+                const data = await fetchJson();
+                const titan = data.destinations[3];
+                fillPlanetData(titan);
+            });
+            break;
+        default:
+            console.log("No matches from switch statement....");
+    }
 });
